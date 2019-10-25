d51c52d501ba25e02b2b3eaac57d55f16e685b38
eduzor kamsi <eduzorkamsi@gmail.com>
2019-10-25 16:22:46 +0100

    designed the interface for the login

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import AppRouter from './AppRouter';
+import Login from './/components/Login';
 
 function App() {
 	return (
 		<div className="App">
+			<Login></Login>
 			<AppRouter></AppRouter>
 		</div>
 	);
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -3,7 +3,6 @@ import React from 'react';
 function LandingPage() {
 	return (
 		<div className="landing-page">
-			<h1>LANDING PAGE</h1>
 		</div>
 	);
 }

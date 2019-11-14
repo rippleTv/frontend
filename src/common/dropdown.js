067e85499cc6ce45c0fb257913a5945a33ffbd4f
Somtozech <ezechinnaemeka@gmail.com>
2019-11-14 23:49:24 +0100

    Added logout to dropdown

--- src/common/dropdown.js
+++ src/common/dropdown.js
@@ -1,23 +1,25 @@
 import React, { Component } from 'react';
-import {Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 class Drop extends Component {
-    
-    render() { 
-        return ( 
-            <div className={`dropdown ${this.props.giveClass}`} >
-                <div  style={{
-                    display:"flex",
-                    flexDirection:"column",
-                    justifyContent: "space-between",
-                    alignItems: "center"
-                    }}>
-                    {this.props.contents.map(content =><div className="clickable drop--items" ><Link to={content.path}>{content.name}</Link></div> )}
-                    
-            </div>
-            </div>
-         );
-    }
+	render() {
+		return (
+			<div className={`dropdown ${this.props.giveClass}`}>
+				<div
+					style={{
+						display: 'flex',
+						flexDirection: 'column',
+						justifyContent: 'space-between',
+						alignItems: 'center'
+					}}
+				>
+					{this.props.children.map(child => (
+						<div className="clickable drop--items">{child}</div>
+					))}
+				</div>
+			</div>
+		);
+	}
 }
- 
+
 export default Drop;
